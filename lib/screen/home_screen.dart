73053abb8b33a5db4c579aca7fbd57e8a8035f69
taewooyo <skaxodn97@gmail.com>
2024-07-10 23:48:52 +0900

    instar feed tab

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,20 +1,187 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
-  State<HomeScreen> createState() => _HomeScreenState();
+  Widget build(BuildContext context) {
+    return const SingleChildScrollView(
+      child: Column(
+        children: [
+          StoryArea(),
+          FeedList(),
+        ],
+      ),
+    );
+  }
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class StoryArea extends StatelessWidget {
+  const StoryArea({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Icon(
-        Icons.home,
-        size: 100,
+    return SingleChildScrollView(
+      scrollDirection: Axis.horizontal,
+      child: Row(
+        children:
+            List.generate(10, (index) => UserStory(userName: 'User $index')),
       ),
+    );
+  }
+}
+
+class UserStory extends StatelessWidget {
+  final String userName;
+
+  const UserStory({
+    required this.userName,
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 12),
+          child: Container(
+            width: 80,
+            height: 80,
+            margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+            decoration: BoxDecoration(
+              color: Colors.blue.shade300,
+              borderRadius: BorderRadius.circular(40),
+            ),
+          ),
+        ),
+        Text(userName),
+      ],
+    );
+  }
+}
+
+class FeedData {
+  final String userName;
+  final int likeCount;
+  final String content;
+
+  FeedData(
+      {required this.userName, required this.likeCount, required this.content});
+}
+
+final feedDataList = [
+  FeedData(userName: 'User1', likeCount: 50, content: '오늘 점심은 맛있었다. 오늘 점심은 맛있었다. 오늘 점심은 맛있었다. 오늘 점심은 맛있었다. 오늘 점심은 맛있었다. 오늘 점심은 맛있었다. 오늘 점심은 맛있었다. 오늘 점심은 맛있었다.'),
+  FeedData(userName: 'User2', likeCount: 24, content: '행복한 하루 보내요!'),
+  FeedData(userName: 'User3', likeCount: 82, content: '오늘 저녁 굿~'),
+  FeedData(userName: 'User4', likeCount: 92, content: '오늘 뭐함?'),
+  FeedData(userName: 'User5', likeCount: 43, content: '반가워요.'),
+  FeedData(userName: 'User6', likeCount: 72, content: '플러터 시작 1일차~'),
+  FeedData(userName: 'User7', likeCount: 3, content: '우와...'),
+  FeedData(userName: 'User8', likeCount: 0, content: '뉴진스 이쁘네요.'),
+  FeedData(userName: 'User9', likeCount: 24, content: '오늘 어디가요?'),
+];
+
+class FeedList extends StatelessWidget {
+  const FeedList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      shrinkWrap: true,
+      physics: const NeverScrollableScrollPhysics(),
+      itemCount: feedDataList.length,
+      itemBuilder: (context, index) => FeedItem(feedData: feedDataList[index]),
+    );
+  }
+}
+
+class FeedItem extends StatelessWidget {
+  final FeedData feedData;
+
+  const FeedItem({required this.feedData, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  Container(
+                    height: 40,
+                    width: 40,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(20),
+                      color: Colors.blue.shade300,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(feedData.userName),
+                ],
+              ),
+              Icon(Icons.more_vert),
+            ],
+          ),
+        ),
+        const SizedBox(
+          height: 8,
+        ),
+        Container(
+          width: double.infinity,
+          height: 300,
+          color: Colors.indigo.shade300,
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 4, horizontal: 12),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                      onPressed: () {}, icon: Icon(Icons.favorite_outline)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(CupertinoIcons.chat_bubble)),
+                  IconButton(
+                      onPressed: () {}, icon: Icon(CupertinoIcons.paperplane)),
+                ],
+              ),
+              IconButton(onPressed: () {}, icon: Icon(CupertinoIcons.bookmark))
+            ],
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24),
+          child: Text(
+            '좋아요 ${feedData.likeCount}개',
+            style: const TextStyle(
+                fontWeight: FontWeight.bold, color: Colors.black),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24,vertical: 4),
+          child: RichText(
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: feedData.userName,
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                TextSpan(text: feedData.content),
+              ],
+              style: const TextStyle(color: Colors.black),
+            ),
+          ),
+        ),
+        const SizedBox(height: 8,)
+      ],
     );
   }
 }
